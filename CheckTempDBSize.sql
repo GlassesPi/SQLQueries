007af5ccbddb360d3d9288fd1c8f789736da48f9
Parsa Rostami <rostami.p@bp.net>
2020-06-28 10:23:55 +0430

    Add CheckTempDBSize.sql & CheckTempTableSize.sql

---
+++ CheckTempDBSize.sql
@@ -0,0 +1,21 @@
+SELECT instance_name AS 'Database',
+[Data File(s) Size (KB)]/1024 AS [Data file (MB)],
+[Log File(s) Size (KB)]/1024 AS [Log file (MB)],
+[Log File(s) Used Size (KB)]/1024 AS [Log file space used (MB)]
+FROM (SELECT * FROM sys.dm_os_performance_counters
+WHERE counter_name IN
+('Data File(s) Size (KB)',
+'Log File(s) Size (KB)',
+'Log File(s) Used Size (KB)')
+AND instance_name = 'tempdb') AS A
+PIVOT
+(MAX(cntr_value) FOR counter_name IN
+([Data File(s) Size (KB)],
+[LOG File(s) Size (KB)],
+[Log File(s) Used Size (KB)])) AS B
+GO
+
+SELECT create_date AS [Creation date],
+recovery_model_desc [Recovery model]
+FROM sys.databases WHERE name = 'tempdb'
+GO
